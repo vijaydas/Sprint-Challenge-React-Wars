328bf770c7ccbd4e479c7d62e07fe86ca0a1738d
Vijay Das <vijaytdas@gmail.com>
2019-04-26 10:33:08 -0500

    laid out file structure, now need to build data from App

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// The character function accepts props coming from the CharacterList tp build the character view
+
+const Character = props => {
+    return (
+
+
+
+    );
+};
+
+
+export default Character;
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Character from "Character"
+
+// Need to build the character array from the starwarsChars array provided bt App
+
